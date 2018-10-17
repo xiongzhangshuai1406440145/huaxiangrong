35b1f2a95f966e111267ae02182a92e7144e8cc3
xiongzhangshuai1406440145 <904926684@qq.com>
2018-10-17 22:37:33 +0800

    js创建

---
+++ js/custom.js
@@ -0,0 +1,23 @@
+/* =========================================================
+****************************Isotope*************************
+============================================================ */
+$.Isotope.prototype._getCenteredMasonryColumns = function() {
+    this.width = this.element.width();
+    
+    var parentWidth = this.element.parent().width();
+    
+                  // i.e. options.masonry && options.masonry.columnWidth
+    var colW = this.options.masonry && this.options.masonry.columnWidth ||
+                  // or use the size of the first item
+                  this.$filteredAtoms.outerWidth(true) ||
+                  // if there's no items, use size of container
+                  parentWidth;
+    
+    var cols = Math.floor( parentWidth / colW );
+    cols = Math.max( cols, 1 );
+
+    // i.e. this.masonry.cols = ....
+    this.masonry.cols = cols;
+    // i.e. this.masonry.columnWidth = ...
+    this.masonry.columnWidth = colW;
+  };
